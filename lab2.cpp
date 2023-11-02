1148404979ab9f0e3c163304483e2a2ef0ca58fd
Quzexuce <149678707+Quzexuce@users.noreply.github.com>
2023-11-02 15:23:44 +0300

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double ds(double x){
+    double y;
+    if (x<-3) y= 1;
+    else if (x<-1) y = -sqrt(4-pow((x+1),2));
+    else if (x<2) y = -2;
+    else y = x-4;
+        
+    return y;
+}
+
+int main(){
+    ofstream out;
+    out.open("textfile.txt");
+    for (double x = -5; x <= 5; x++) out << "x = " << x <<"\ty =" << ds(x) << endl;
+    out.close();
+    ifstream in("textfile.txt");
+    string line;
+    while (getline(in,line)) cout << line << endl;    
+    return 0;
+}
